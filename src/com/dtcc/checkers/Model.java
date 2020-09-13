f2cc5a9378004cc397e813fd3f32bacd647be5a8
koastiebratt@gmail.com <you@example.com>
2020-09-13 13:34:12 -0400

    Save, load, defualt board save file, updated to-do

--- src/com/dtcc/checkers/Model.java
+++ src/com/dtcc/checkers/Model.java
@@ -1,7 +1,14 @@
 package com.dtcc.checkers;
 
-public class Model {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class Model {
+	
+	private final static String File = "src\\com\\dtcc\\checkers\\Checkers_Save_File.tx";
+	
     public String[][] update(Move move){
         return  null;
     }
@@ -10,7 +17,48 @@ public class Model {
         return null;
     }
 
-    public void save(){}
+    public void save(){
+		try {
+			
+			PrintWriter out = new PrintWriter(new File(File));
+			
+			String board[][] = getBoard();
+			
+			//Rows
+			for(int y = 0; y < board.length; y++) {
+				//Columns
+				for(int x = 0; x < board[x].length; x++) {
+					out.print(board[y][x] + " ");
+				}
+				out.println();
+			}
+			}
+		catch(FileNotFoundException e) {
+			
+		}
 
-    public void load(){}
+    }
+
+    public static void load(){
+    	try {
+    		int temp_board[][] = new int[8][8];
+			Scanner input = new Scanner(new File(File));
+			//Only runs while 
+			while(input.hasNextInt()) {
+				//Rows
+				for(int y = 0; y < temp_board.length; y++) {
+					//Columns
+					for(int x = 0; x < temp_board.length; x++) {
+						temp_board[y][x] = input.nextInt();
+					}
+				}
+			}
+			
+			//Do something with temp_board, not sure yet.
+			
+		}
+		catch(FileNotFoundException e) {
+			
+		}
+    }
 }
