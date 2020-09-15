e0f205de15b028e0fe724c5a513e38b918e4324a
koastiebratt@gmail.com <you@example.com>
2020-09-14 20:52:07 -0400

    Flopped em

--- src/com/dtcc/checkers/Model.java
+++ src/com/dtcc/checkers/Model.java
@@ -17,7 +17,28 @@ public class Model {
     public String[][] getBoard() {
         return null;
     }
-
+    
+    public static void save(String board[][]){
+		try {
+			PrintWriter out = new PrintWriter(new File(File));
+			String temp_board[][] = board;
+			System.out.println(temp_board.length);
+			//Rows
+			for(int y = 0; y < temp_board.length; y++) {
+				//Columns
+				for(int x = 0; x < temp_board.length; x++) {
+					out.print(temp_board[y][x] + " ");
+					System.out.print(temp_board[y][x] + " ");
+				}
+				out.println();
+			}
+			out.close();
+			}
+		catch(FileNotFoundException e) {
+			//Do something
+		}
+    }
+    
     public static void load(){
     	try {
     		String temp_board[][] = new String[8][8];
@@ -37,27 +58,6 @@ public class Model {
 		}
 		catch(FileNotFoundException e) {
 			//Do Something
-		}
-    }
-	
-	public static void save(String board[][]){
-		try {
-			PrintWriter out = new PrintWriter(new File(File));
-			String temp_board[][] = board;
-			System.out.println(temp_board.length);
-			//Rows
-			for(int y = 0; y < temp_board.length; y++) {
-				//Columns
-				for(int x = 0; x < temp_board.length; x++) {
-					out.print(temp_board[y][x] + " ");
-					System.out.print(temp_board[y][x] + " ");
-				}
-				out.println();
-			}
-			out.close();
-			}
-		catch(FileNotFoundException e) {
-			//Do something
 		}
     }
 }
